e41cc3243726f8d544db6506b8ec488b26df91b2
Fülöp Dániel <fulopdaniel@gmail.com>
2019-09-21 11:52:58 +0200

    Tokeletes szam kesz

--- TokeletesSzam/src/Main.java
+++ TokeletesSzam/src/Main.java
@@ -3,6 +3,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int szam = 6;
+		int osztokOsszege=0;
+		for (int i = 1; i < szam; i++) {
+			
+			if (szam % i == 0) {
+				osztokOsszege += i;
+			}
+			
+		}
+		
+		if (osztokOsszege == szam) {
+			System.out.println("Tokeletes szam");
+		}
 
 	}
 
