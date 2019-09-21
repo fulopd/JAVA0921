723329f40fa0371e80b0c8d34b716caec3159fa1
Fülöp Dániel <fulopdaniel@gmail.com>
2019-09-21 11:46:43 +0200

    lepcsozetes

--- Primszame/src/Main.java
+++ Primszame/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int szam = 3;
+		int szam = 3499;
 		int szamlalo = 0;
 		System.out.println("A vizsgált szám: " + szam);
 		for (int i = 1; i <= szam; i++) {
