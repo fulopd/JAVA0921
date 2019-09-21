abd9448ca6a5195a64fbce11f10e6005289f54c6
Fülöp Dániel <fulopdaniel@gmail.com>
2019-09-21 11:30:50 +0200

    Prím szám e. Kész

--- Primszame/src/Main.java
+++ Primszame/src/Main.java
@@ -4,7 +4,21 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int szam = 3;
+		int szamlalo = 0;
 		
+		for (int i = 1; i <= szam; i++) {
+			
+			if (szam % i == 0) {
+				szamlalo++;
+			}
+		}
+		
+		if (szamlalo == 2) {
+			System.out.println("Prím szám ");
+		}else {
+			System.out.println("Nem pírm");
+		}
 		
 	}
 
